6760778310755b2bdbeec7d14f1ff6a1d22fcfae
Ales-good <ales-good@mail.ru>
2025-11-04 16:49:36 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,12 @@
 echo "Setting up environment..."
 
 # Создаем необходимые директории
-mkdir -p models logs data/processed_data data/raw_data
+mkdir -p models logs data
 
 # Устанавливаем права
 chmod +x main.py
+
+# Создаем необходимые файлы если их нет
+touch bot.log
 
 echo "Setup complete!"
